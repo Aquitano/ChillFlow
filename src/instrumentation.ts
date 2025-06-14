f5086b5cfb8946484230581de2f90cf6a3d1c1ac
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2025-06-14 23:11:01 +0200

    chore: update deps

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,12 +2,12 @@ import { captureRequestError } from '@sentry/nextjs';
 
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
-        await import('../sentry.server.config');
+        await import('../instrumentation-server');
     }
 
-    if (process.env.NEXT_RUNTIME === 'edge') {
-        await import('../sentry.edge.config');
-    }
+    // if (process.env.NEXT_RUNTIME === 'edge') {
+    //     await import('../sentry.edge.config');
+    // }
 }
 
 export const onRequestError = captureRequestError;
